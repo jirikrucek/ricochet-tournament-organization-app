b4f4ab4149761f588113978388f414984fcfc744
Jiří Krůček <jiri.krucek@gmail.com>
2026-03-25 16:14:42 +0100

    fix(vitest): correct syntax error in setupFiles configuration

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'jsdom',
-    setupFiles: './src/setupTests.js'q
+    setupFiles: './src/setupTests.js'
   }
 });
